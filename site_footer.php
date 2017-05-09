d9525afcff0b48d40b8f344ab5a28d428576ab75
zoemaurer <maurerzj@mx.lakeforest.edu>
2017-05-09 18:25:19 -0500

    Add files via upload

---
+++ site_footer.php
@@ -0,0 +1,12 @@
+</div>
+<div id="footer">
+         <h3>
+         <p1>Copyright 2017 Zoe Maurer</p1></h3>
+         </div>
+
+</div>
+</body>
+<footer>
+</footer>
+</body>
+</html>
---
+++ site_header.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="icon" href="favicon.ico" type="image/x-icon"/>
+<link rel="shortcut icon" href="favicon.ico" type="image/x-icon"/>
+<meta charset="UTF-8">
+  <script language="javascript" type="text/javascript" src="libraries/p5.js"></script>
+  <!-- uncomment lines below to include extra p5 libraries -->
+  <!--<script language="javascript" src="libraries/p5.dom.js"></script>-->
+  <!--<script language="javascript" src="libraries/p5.sound.js"></script>-->
+  <script language="javascript" type="text/javascript" src="site_header.php"></script>
+  <script language="javascript" type="text/javascript" src="site_footer.php"></script>
+  <script language="javascript" type="text/javascript" src="style.css"></script>
+
+
+  <!-- this line removes any default padding and style. you might only need one of these values set. -->
+<meta name = "Zoes Senior Portfolio Website" content = "assignments">
+<meta name = "design" content = "artwork">
+<title>Zoe's Senior Portfolio</title>
+<center>
+</head>
+<body bgcolor= "D3D3D3">
+<center>
+<header>
+<link rel="stylesheet" type="text/css" href="mystyle.css">
+
+</head>
+<body>
+
+
+<div id="page_wrapper">
+         <div id="header">
+         <h1><td><img src="favicon.jpg" alt="favicon" width="50" height="50" align="left"></td>
+
+        	<p>Zoe's Senior Portfolio Project</p>
+         <nav id="nav_bar">
+
+
+<ul class="main links">
+<li><a href="about.php">About</a></li>
+<li><a href="projects.php">My Projects</a></li>
+
+
+
+
+
+
+</ul class="main links"></h1>
+
+
+
+         </div>
+
+         <div id="content">
